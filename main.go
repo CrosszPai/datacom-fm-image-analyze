7d9fe4f1649e8242449f6b03cf2f9b1a12db7cbe
CrosszPai <61010556@kmitl.ac.th>
2019-11-22 19:09:59 +0700

    add comment to program

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 )
 
+// check if file is appear in folder but not sure that file is complete writing .
 func checkFileExist(filename string) bool {
 	if _, err := os.Stat(filename + ".bmp"); err != nil {
 		if os.IsNotExist(err) {
@@ -20,6 +21,8 @@ func checkFileExist(filename string) bool {
 	return true
 }
 
+// read bit map image file and process it into [4][4]bool
+// return true if image match with some pattern and  2 dimension position array that  has true value if color is black
 func readimage(s string) (bool, [4][4]bool) {
 	reader, err := os.Open(s + ".bmp")
 	if err != nil {
@@ -45,9 +48,11 @@ func readimage(s string) (bool, [4][4]bool) {
 	out, _ := os.Create(strings.Split(s, "/")[0] + "/" + "_4x4_" + strings.Split(s, "/")[1] + ".bmp")
 	defer out.Close()
 	_, _ = out.Write(lpp.Bytes())
+	// black value threshold
 	black := []uint8{41, 41, 41}
 	//white := []uint8{255, 255, 255}
 	var pos [4][4]bool
+	// map pixel which color value which is higher than black value threshold
 	for y := 0; y < 4; y++ {
 		for x := 0; x < 4; x++ {
 			r, g, b, _ := newImage.At(x, y).RGBA()
@@ -68,7 +73,7 @@ func readimage(s string) (bool, [4][4]bool) {
 		}
 		//fmt.Println("")
 	}
-
+	// process image
 	if !pos[0][1] && !pos[0][2] &&
 		!pos[1][3] && !pos[2][3] &&
 		pos[3][1] && pos[3][2] &&
@@ -116,6 +121,7 @@ func readimage(s string) (bool, [4][4]bool) {
 
 func main() {
 	start := 0
+	// setup config
 	//serialCofig := &serial.Config{
 	//	Name: os.Getenv("DATACOM_PORT"),
 	//	Baud: 115209,
@@ -125,18 +131,21 @@ func main() {
 	//if err != nil {
 	//	log.Fatalf("Error occur at open serial port")
 	//}
+
+	// main program loop
 	for {
 		dirpath := "out/" + strconv.Itoa(start)
 		if checkFileExist(dirpath) {
 			fmt.Printf("Found image No. %v \n", start)
 			fmt.Println("waiting complete image")
+			//loop until get valid file
 			for testfile, err := os.Open(dirpath + ".bmp"); err != nil; {
 				_ = testfile.Close()
 				log.Printf("chk file %v ,%v", testfile, err)
 			}
 			fmt.Println("image complete saved")
 			if found, pos := readimage(dirpath); found == true {
-				// send response to arduino via serial
+				// send data response to arduino via serial
 				for row := range pos {
 					buffer := make([]byte, 4)
 					for col := range pos[row] {
@@ -146,14 +155,15 @@ func main() {
 							buffer[col] = 0
 						}
 					}
+					// write 1 image row buffer
 					//for _,err:=s.Write(buffer);err != nil;{
 					//}
 					fmt.Printf("%v\n", buffer)
-
 				}
 			}
+			// increase number of file
 			start++
-			fmt.Println("Process success waiting for next file.")
+			fmt.Println("Process success waiting for next file . . .")
 			fmt.Println("==========================")
 		}
 	}
